18f6414af23b854a3cbdd66a90ae1e4fcc5ff849
Bram <bram@bram.codes>
2018-05-08 16:58:22 +0200

    Add support for future timestamps

--- index.js
+++ index.js
@@ -2,26 +2,32 @@
 function format(date) {
 	var diff = Date.now() - date.getTime();
 
-	if (diff < 10000) {
-		return 'just now';
-	}
+	var future = diff < 0;
+	diff = Math.abs(diff);
+
+	if (!future && diff < 10000) return 'just now';
+	if (future && diff < 5000) return 'any second';
+
+	var suffix = future ? ' from now' : ' ago';
 
 	for (var i = 0; i < units.length; i++) {
-		if (diff <= units[i].max * units[i].value) {
-			var t = Math.round(diff / units[i].value);
-			return t <= 1 ? units[i].single : t + ' ' + units[i].name + 's ago';
+		var unit = units[i];
+
+		if (diff <= unit.max * unit.value) {
+			var t = Math.round(diff / unit.value);
+			return t === 1 ? unit.single + suffix : t + unit.name + 's' + suffix;
 		}
 	}
 }
 
 var units = [
-	{ name: 'second', value: 1000, max: 50, single: 'just now' },
-	{ name: 'minute', value: 60000, max: 50, single: 'a minute ago' },
-	{ name: 'hour', value: 3600000, max: 22, single: 'an hour ago' },
-	{ name: 'day', value: 86400000, max: 6, single: 'yesterday' },
-	{ name: 'week', value: 604800000, max: 3.5, single: 'last week' },
-	{ name: 'month', value: 2592000000, max: 11, single: 'last month' },
-	{ name: 'year', value: 31536000000, max: Infinity, single: 'a year ago' }
+	{ name: ' second', value: 1000, max: 50, single: 'a second' },
+	{ name: ' minute', value: 60000, max: 50, single: 'a minute' },
+	{ name: ' hour', value: 3600000, max: 22, single: 'an hour' },
+	{ name: ' day', value: 86400000, max: 6, single: 'a day' },
+	{ name: ' week', value: 604800000, max: 3.5, single: 'a week' },
+	{ name: ' month', value: 2592000000, max: 11, single: 'a month' },
+	{ name: ' year', value: 31536000000, max: Infinity, single: 'a year' }
 ];
 
 module.exports = format;
--- test.js
+++ test.js
@@ -1,73 +1,116 @@
 import test from 'ava';
 import friendlyTime from './index.js'
 
-const now = new Date();
+const now = Date.now();
 
 test('second', t => {
-	t.is(friendlyTime(now), 'just now');
+	const time = new Date(now - (1000 * 5));
+	t.is(friendlyTime(time), 'just now');
+
+	const time2 = new Date(now + (1000 * 3));
+	t.is(friendlyTime(time2), 'any second');
 });
 
 test('seconds', t => {
 	const time = new Date(now - (1000 * 15));
 	t.is(friendlyTime(time), '15 seconds ago');
+
+	const time2 = new Date(now + (1000 * 40));
+	t.is(friendlyTime(time2), '40 seconds from now');
 });
 
 test('minute', t => {
 	const time = new Date(now - (1000 * 60));
 	t.is(friendlyTime(time), 'a minute ago');
+
+	const time2 = new Date(now + (1000 * 70));
+	t.is(friendlyTime(time2), 'a minute from now');
 });
 
 test('minutes', t => {
 	const time = new Date(now - (1000 * 60 * 30));
 	t.is(friendlyTime(time), '30 minutes ago');
+
+	const time2 = new Date(now + (1000 * 60 * 12));
+	t.is(friendlyTime(time2), '12 minutes from now');
 });
 
 test('hour', t => {
 	const time = new Date(now - (1000 * 60 * 60));
 	t.is(friendlyTime(time), 'an hour ago');
+
+	const time2 = new Date(now + (1000 * 60 * 90));
+	t.is(friendlyTime(time2), 'an hour from now');
 });
 
 test('hours', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 2));
 	t.is(friendlyTime(time), '2 hours ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 6));
+	t.is(friendlyTime(time2), '6 hours from now');
 });
 
 test('day', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24));
-	t.is(friendlyTime(time), 'yesterday');
+	t.is(friendlyTime(time), 'a day ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 23));
+	t.is(friendlyTime(time2), 'a day from now');
 });
 
 test('days', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 5));
 	t.is(friendlyTime(time), '5 days ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 6));
+	t.is(friendlyTime(time2), '6 days from now');
 });
 
 test('week', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 7));
-	t.is(friendlyTime(time), 'last week');
+	t.is(friendlyTime(time), 'a week ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 9));
+	t.is(friendlyTime(time2), 'a week from now');
 });
 
 test('weeks', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 7 * 3));
 	t.is(friendlyTime(time), '3 weeks ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 7 * 2));
+	t.is(friendlyTime(time2), '2 weeks from now');
 });
 
 test('month', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 31));
-	t.is(friendlyTime(time), 'last month');
+	t.is(friendlyTime(time), 'a month ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 29));
+	t.is(friendlyTime(time2), 'a month from now');
 });
 
 test('months', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 30 * 9));
 	t.is(friendlyTime(time), '9 months ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 30 * 2));
+	t.is(friendlyTime(time2), '2 months from now');
 });
 
 test('year', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 366));
 	t.is(friendlyTime(time), 'a year ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 360));
+	t.is(friendlyTime(time2), 'a year from now');
 });
 
 test('years', t => {
 	const time = new Date(now - (1000 * 60 * 60 * 24 * 365 * 12));
 	t.is(friendlyTime(time), '12 years ago');
+
+	const time2 = new Date(now + (1000 * 60 * 60 * 24 * 365 * 40));
+	t.is(friendlyTime(time2), '40 years from now');
 });
